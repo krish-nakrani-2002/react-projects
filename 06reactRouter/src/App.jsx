faf497daa2a7a2fe089436e87200ec4554da6a24
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-20 16:40:35 +0530

    add github components in header

---
+++ 06reactRouter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Components/Home/home'
+import Footer from './Components/Footer/footer'
+import Header from './Components/Header/header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
