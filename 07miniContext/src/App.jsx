e80979279bc65221a353eaa536cf37c5c4dfb521
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-24 11:13:22 +0530

    login component

--- 07miniContext/src/App.jsx
+++ 07miniContext/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
     <h1>Krish Nakrani</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
