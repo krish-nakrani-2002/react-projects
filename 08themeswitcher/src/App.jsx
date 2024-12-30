a651d1c8cfb48460d7a103d37aa4d030c58cb000
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-30 20:29:01 +0530

    create theme context

--- 08themeswitcher/src/App.jsx
+++ 08themeswitcher/src/App.jsx
@@ -1,11 +1,22 @@
 
+import { useState } from 'react';
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
+  const {themeMode, setThemeMode} = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
 
   return (
-  
-<div className="flex flex-wrap min-h-screen items-center">
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
               {/* themeBtn */}
@@ -15,8 +26,8 @@ function App() {
                {/* Card */}
         </div>
     </div>
-</div>
-
+    </div>
+  </ThemeProvider>
   )
 }
 
